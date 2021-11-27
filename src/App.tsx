a1e87337c81f8186a9c860e1c4e56c0ebaab46f9
saltro <2373117747@qq.com>
2021-11-27 23:55:44 +0800

    init: 项目初始化

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello React + TypeScript!</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path'); // 导入path模块
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // 导入html-webpack-plugin模块
+
+module.exports = {
+  // 入口文件
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/, // 排除 node_modules 目录
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'], // 自动解析确定的扩展
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './template/index.html'), // 模板位置
+      filename: 'index.html', // 输出后的文件名，路径是 output.path
+      title: 'Nano', // 传给模板的变量
+    }),
+  ],
+  // 出口文件
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  cache: {
+    type: 'filesystem',
+    buildDependencies: {
+      config: [__filename], // 构建依赖的config文件（通过 require 依赖）内容发生变化时，缓存失效
+    },
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'), // 告诉服务器从指定目录中提供静态文件，也即打包后的文件
+    },
+    port: 8080, // 设置端口
+    open: true, // 自动打开浏览器
+    hot: true, // 开启热更新
+  },
+};
