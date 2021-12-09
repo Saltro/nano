bb6524b99a9146e12943d274d41ab7c51ecf4855
NagisaCo <yjr888@vip.qq.com>
2021-12-09 19:10:13 +0800

    feat(TypeChoose): 增加类型切换

--- src/components/TypeChoose/index.tsx
+++ src/components/TypeChoose/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './index.less';
 import TypeChooseItem from '@/components/TypeChoose/TypeChooseItem';
 
@@ -12,13 +12,14 @@ interface IProps {
 const TypeChoose: React.FC<IProps> = (props) => {
   const { itemList } = props;
 
-  let chooseId = 1;
+  const [chooseId, setChooseId] = useState(1);
 
   return (
     <div id={style.container}>
       {itemList.map((item) => {
         return (
-          <div className={item.id === chooseId ? style.typeChooseItemChosen : style.typeChooseItem} key={item.id}>
+          <div className={item.id === chooseId ? style.typeChooseItemChosen : style.typeChooseItem} key={item.id}
+               onClick={() => setChooseId(item.id)}>
             <TypeChooseItem {...item} />
           </div>
         );
