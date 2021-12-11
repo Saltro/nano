0d3900a05a5b2272e743e93bef8099fcbd27ced2
wzkMaster <727498469@qq.com>
2021-12-11 21:35:24 +0800

    feat: 详情页引入地图组件

--- src/views/Details/index.tsx
+++ src/views/Details/index.tsx
@@ -1,11 +1,10 @@
 import HomeLayout from '@/layouts/HomeLayout';
 import LocTable from '@/components/LocTable';
+import Map from '@/components/Map';
 import DoubleSwiper from '@/components/DoubleSwiper';
 import Detail from '@/components/Detail';
 import React from 'react';
 import style from './index.less';
-
-
 
 export default function Details() {
   
@@ -14,13 +13,19 @@ export default function Details() {
       <div className={style.container}>
         <Detail/>
        <div className={style.gallery}>
-          <div className={style.galleryTitle}>
+          <div className={style.title}>
             <h2>实景照片</h2>
             <DoubleSwiper/>
           </div>
         </div>
-        <div>
-          <div className={style.locsTable}>
+        <div className={style.map}>
+          <div className={style.title}>
+            <h2>朝圣地图</h2>
+          </div>  
+          <Map/>
+        </div>
+        <div className={style.locations}>
+          <div className={style.title}>
             <h2>地址详情</h2>
           </div>  
           <LocTable/>
