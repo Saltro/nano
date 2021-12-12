039d5c1a659c2bb98d5878cacc00cea3cc504506
NagisaCo <yjr888@vip.qq.com>
2021-12-12 23:55:29 +0800

    perf(PageController): 箭头使用icon

--- src/components/PageController/index.tsx
+++ src/components/PageController/index.tsx
@@ -39,7 +39,21 @@ const PageController: React.FC<{}> = () => {
           className={style.pageControllerContentButton}
           onClick={() => handleCurrentPage(currentPage - 1)}
         >
-          <PageButton>←</PageButton>
+          <PageButton>
+            <span role="img" aria-label="arrow-left" className="anticon anticon-arrow-left">
+              <svg
+                viewBox="64 64 896 896"
+                focusable="false"
+                data-icon="arrow-left"
+                width="1em"
+                height="1em"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path d="M872 474H286.9l350.2-304c5.6-4.9 2.2-14-5.2-14h-88.5c-3.9 0-7.6 1.4-10.5 3.9L155 487.8a31.96 31.96 0 000 48.3L535.1 866c1.5 1.3 3.3 2 5.2 2h91.5c7.4 0 10.8-9.2 5.2-14L286.9 550H872c4.4 0 8-3.6 8-8v-60c0-4.4-3.6-8-8-8z" />
+              </svg>
+            </span>
+          </PageButton>
         </div>
 
         <div
@@ -74,7 +88,21 @@ const PageController: React.FC<{}> = () => {
           className={style.pageControllerContentButton}
           onClick={() => handleCurrentPage(currentPage + 1)}
         >
-          <PageButton>→</PageButton>
+          <PageButton>
+            <span role="img" aria-label="arrow-right" className="anticon anticon-arrow-right">
+              <svg
+                viewBox="64 64 896 896"
+                focusable="false"
+                data-icon="arrow-right"
+                width="1em"
+                height="1em"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path d="M869 487.8L491.2 159.9c-2.9-2.5-6.6-3.9-10.5-3.9h-88.5c-7.4 0-10.8 9.2-5.2 14l350.2 304H152c-4.4 0-8 3.6-8 8v60c0 4.4 3.6 8 8 8h585.1L386.9 854c-5.6 4.9-2.2 14 5.2 14h91.5c1.9 0 3.8-.7 5.2-2L869 536.2a32.07 32.07 0 000-48.4z" />
+              </svg>
+            </span>
+          </PageButton>
         </div>
       </div>
       <div className={style.pageControllerNum}>共 {workContext.totalPages} 页</div>
