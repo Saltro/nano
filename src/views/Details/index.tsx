6b6e6543d46ac03704386de1c548d8f0b7a6d9c6
wzkMaster <727498469@qq.com>
2021-12-12 11:09:30 +0800

    引入标题组件

--- src/views/Details/index.tsx
+++ src/views/Details/index.tsx
@@ -3,6 +3,7 @@ import LocTable from '@/components/LocTable';
 import Map from '@/components/Map';
 import DoubleSwiper from '@/components/DoubleSwiper';
 import Detail from '@/components/Detail';
+import Title from '@/components/Title';
 import React from 'react';
 import style from './index.less';
 
@@ -13,21 +14,15 @@ export default function Details() {
       <div className={style.container}>
         <Detail/>
        <div className={style.gallery}>
-          <div className={style.title}>
-            <h2>实景照片</h2>
-            <DoubleSwiper/>
-          </div>
+          <Title text="实景照片"/>
+          <DoubleSwiper/>
         </div>
         <div className={style.map}>
-          <div className={style.title}>
-            <h2>朝圣地图</h2>
-          </div>  
+          <Title text="朝圣地图"/> 
           <Map/>
         </div>
         <div className={style.locations}>
-          <div className={style.title}>
-            <h2>地址详情</h2>
-          </div>  
+          <Title text="地址详情"/>
           <LocTable/>
         </div>
       </div>
