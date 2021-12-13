b358d378f3390c0b02175e08b37c4812c12a5018
NagisaCo <yjr888@vip.qq.com>
2021-12-13 23:57:03 +0800

    refactor(Home): 完善css样式

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import HomeLayout from '@/layouts/HomeLayout';
 import style from './index.less';
+import RecommendationTable from '@/components/RecommendationTable';
+import RecommendationContainer from '@/context/RecommendationContainer';
 import { Link } from 'react-router-dom';
+import PageController from '@/components/PageController';
 
 const Home: React.FC<{}> = () => {
   return (
     <HomeLayout>
-      <div id={style.container}>这里是主页中间部分</div>
-      <Link to="/login">登录</Link>
-      <Link to="/details">详情</Link>
+      <RecommendationContainer>
+        <div id={style.container}>
+          <RecommendationTable />
+          <PageController />
+        </div>
+      </RecommendationContainer>
+      <div style={{ position: 'absolute' }}>
+        <Link to="/login">登录</Link>
+        <Link to="/details">详情</Link>
+      </div>
     </HomeLayout>
   );
 };
