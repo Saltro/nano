62fe1b0fc060dc24958ffaa367b4523930d7b4e6
NagisaCo <yjr888@vip.qq.com>
2021-12-15 23:29:47 +0800

    feat(WorkContext): 添加search关键词

--- src/context/WorkContainer/index.tsx
+++ src/context/WorkContainer/index.tsx
@@ -14,9 +14,10 @@ const WorkContainer: React.FC<{}> = ({ children }) => {
   const [totalPages, setTotalPages] = React.useState<number>(1);
   const [typeId, setTypeId] = React.useState<number>(1);
   const [itemList, setItemList] = React.useState<IWorkInfo[]>([]);
+  const [searchKey, setSearchKey] = React.useState<string>('');
 
   useEffect(() => {
-    Request.getWorkList(typeId, currentPage)
+    Request.getWorkList(typeId, currentPage, searchKey)
       .then((res) => {
         console.log('获取WorkList成功', res);
         setTotalPages(Math.ceil(res.count / 20));
@@ -44,8 +45,10 @@ const WorkContainer: React.FC<{}> = ({ children }) => {
       typeId,
       setTypeId: handleTypeIdChange,
       itemList,
+      searchKey,
+      setSearchKey,
     };
-  }, [currentPage, totalPages, typeId, itemList]);
+  }, [currentPage, totalPages, typeId, itemList, searchKey]);
 
   return <WorkContext.Provider value={value}>{children}</WorkContext.Provider>;
 };
