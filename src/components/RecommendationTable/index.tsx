e66163952e10f4aae52ab1437973e53e54d39e42
NagisaCo <yjr888@vip.qq.com>
2021-12-16 23:33:52 +0800

    perf(RecoContainer): 分离请求与context

--- src/components/RecommendationTable/index.tsx
+++ src/components/RecommendationTable/index.tsx
@@ -1,16 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import style from './index.less';
-import RecommendationItem from '@/components/RecommendationTable/RecommendationItem';
+import RecommendationItem, { IRecommendationInfo } from '@/components/RecommendationTable/RecommendationItem';
 import { useRecommendationContext } from '@/context/RecommendationContainer';
+import Request from '@/request';
 
 const RecommendationTable: React.FC<{}> = () => {
-  const recommendationContext = useRecommendationContext();
+  const {currentPage, totalPages, setTotalPages, setCurrentPage} = useRecommendationContext();
 
-  const [recommendationList, setRecommendationList] = useState(recommendationContext.itemList);
+  const [recommendationList, setRecommendationList] = useState<IRecommendationInfo[]>([]);
+
+  const refreshRecommendationList = () => {
+    console.log({ currentPage: currentPage, totalPages: totalPages });
+    Request.getRecommendationList(1) // TODO: 数据不够，暂时只请求第一页
+      .then((res) => {
+        console.log('获取RecommendationList成功', res);
+        // setTotalPages(Math.ceil(res.count / 20));
+        setTotalPages(16); // TODO: 假设有好多页数据
+        setRecommendationList(res.results);
+        setCurrentPage(1);
+      })
+      .catch((err) => {
+        console.log('获取RecommendationList失败', err);
+      });
+  };
 
   useEffect(() => {
-    setRecommendationList(recommendationContext.itemList);
-  }, [recommendationContext.itemList]);
+    // 初始化加载
+    refreshRecommendationList();
+  }, []);
 
   return (
     <div id={style.container}>
