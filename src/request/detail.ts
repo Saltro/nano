8c9f836fb10aa4f0056b4522e89fd8574fbf6c5c
wzkMaster <727498469@qq.com>
2021-12-16 21:45:41 +0800

    fix: 修改getPlace接受的参数类型

--- src/request/detail.ts
+++ src/request/detail.ts
@@ -1,22 +1,5 @@
 import { request } from '@/request/instance';
 
-// interface IAnimeDetail{
-//     title: string,
-//     titleCN: string,
-//     cover: string,
-//     images: string,
-//     description: string,
-//     director: string,
-//     screenwriter: string,
-//     actors: string[],
-//     categories: string[],
-//     website: string,
-//     country: string,
-//     date: string,
-//     imdb: string,
-//     alias: string[]
-// }
-
 const getAnimeDetail = async (id: string | undefined) => {
     const res = await request.get(`/anime/${id}`).then((res) => {
         return res.data;
@@ -24,7 +7,7 @@ const getAnimeDetail = async (id: string | undefined) => {
     return res;
 }
 
-const getPlaces = async (id: number) => {
+const getPlaces = async (id: string | undefined) => {
     const res = await request.get(`/place/?anime_id=${id}`).then(res => {
         return res.data
     });
