5f67d37cad6c263ed2da4d93765e4e6304e38f12
saltro <2373117747@qq.com>
2021-12-19 15:49:35 +0800

    build(webpack): 将路由模式换为hash；修改webpack解决开发时404问题

--- webpack.config.js
+++ webpack.config.js
@@ -114,5 +114,6 @@ module.exports = {
     port: 8080, // 设置端口
     open: true, // 自动打开浏览器
     hot: true, // 开启热更新
+    historyApiFallback: true, // 当使用 HTML5 History API 时，任意的 404 响应都可能需要被替代为 index.html
   },
 };
