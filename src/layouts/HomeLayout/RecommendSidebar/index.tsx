a5009f2dbaec875e9ea70b3e2f523a2c5db21c7f
saltro <2373117747@qq.com>
2021-12-21 21:28:58 +0800

    refactor(recommendsidebar): 更新推荐侧栏图床引入；修改Image组件位置

--- src/layouts/HomeLayout/RecommendSidebar/index.tsx
+++ src/layouts/HomeLayout/RecommendSidebar/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Image from './component/Image';
+import Image from './Image';
 import style from './index.less';
 
 const RecommendSidebar: React.FC<{}> = () => {
@@ -11,15 +11,15 @@ const RecommendSidebar: React.FC<{}> = () => {
         <div className={style.recommend}>
           <Image
             title="寺庙、神社与祭典"
-            src="https://github.com/Saltro/nano/blob/master/src/assets/images/hotSpot1.jpg?raw=true"
+            src="http://qiniu-picgo.saltroping.com/hotSpot1.jpg"
           />
           <Image
             title="秋叶原"
-            src="https://github.com/Saltro/nano/blob/master/src/assets/images/hotSpot2.jpg?raw=true"
+            src="http://qiniu-picgo.saltroping.com/hotSpot2.jpg"
           />
           <Image
             title="走进新海诚"
-            src="https://github.com/Saltro/nano/blob/master/src/assets/images/hotSpot3.jpg?raw=true"
+            src="http://qiniu-picgo.saltroping.com/hotSpot3.jpg"
           />
         </div>
       </div>
@@ -27,19 +27,19 @@ const RecommendSidebar: React.FC<{}> = () => {
         <span className={style.title}>城市推荐</span>
         <span className={style.arrow}>→</span>
         <div className={style.cityImages}>
-          <Image title="东京" src="https://github.com/Saltro/nano/blob/master/src/assets/images/city1.jpg?raw=true" />
-          <Image title="京都" src="https://github.com/Saltro/nano/blob/master/src/assets/images/city3.jpg?raw=true" />
-          <Image title="镰仓" src="https://github.com/Saltro/nano/blob/master/src/assets/images/city2.jpg?raw=true" />
+          <Image title="东京" src="http://qiniu-picgo.saltroping.com/city1.jpg" />
+          <Image title="京都" src="http://qiniu-picgo.saltroping.com/city2.jpg" />
+          <Image title="镰仓" src="http://qiniu-picgo.saltroping.com/city3.jpg" />
         </div>
       </div>
       <div className={style.strategy}>
         <span className={style.title}>圣地巡礼攻略</span>
         <span className={style.arrow}>→</span>
         <div className={style.images}>
-          <Image src="https://github.com/Saltro/nano/blob/master/src/assets/images/%E8%B7%AF%E7%BA%BF.png?raw=true" />
-          <Image src="https://github.com/Saltro/nano/blob/master/src/assets/images/%E4%BA%A4%E9%80%9A.png?raw=true" />
-          <Image src="https://github.com/Saltro/nano/blob/master/src/assets/images/%E5%AD%A3%E8%8A%82.png?raw=true" />
-          <Image src="https://github.com/Saltro/nano/blob/master/src/assets/images/%E6%8A%80%E5%B7%A7.png?raw=true" />
+          <Image src="http://qiniu-picgo.saltroping.com/路线.png" />
+          <Image src="http://qiniu-picgo.saltroping.com/交通.png" />
+          <Image src="http://qiniu-picgo.saltroping.com/季节.png" />
+          <Image src="http://qiniu-picgo.saltroping.com/技巧.png" />
         </div>
       </div>
     </div>
