aca8cf16b6c5eb3dbc2c11fc804e634a38983eb3
wzkMaster <727498469@qq.com>
2021-12-21 22:40:22 +0800

    feat(Places): 根据路径中的参数展现不同中心点的地图

--- src/views/Places/index.tsx
+++ src/views/Places/index.tsx
@@ -1,26 +1,24 @@
 import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import style from './index.less';
 import Request from '@/request';
 import Map from '@/components/Map';
 import HomeLayout from '@/layouts/HomeLayout';
 
 const Places: React.FC<{}> = () => {
-  const [places, setPlaces] = useState<IPlaceInfoBrief[]>([]);
+  const [place, setPlace] = useState<IPlaceInfoBrief>();
+  const { id } = useParams();
   useEffect(() => {
-    Request.getPlaces().then((res) => {
+    Request.getPlace(id).then((res) => {
       const { data } = res;
-      setPlaces(
-        data.results.map((place) => {
-          return {
-            id: place.id,
-            name: place.name,
-            address: place.address,
-            longitude: place.longitude,
-            latitude: place.latitude,
-            photos: place.photos,
-          };
-        }),
-      );
+      setPlace({
+        id: data.id,
+        name: data.name,
+        address: data.address,
+        longitude: data.longitude,
+        latitude: data.latitude,
+        photos: data.photos,
+      });
     });
   }, []);
 
@@ -28,10 +26,10 @@ const Places: React.FC<{}> = () => {
     <HomeLayout>
       <div className={style.container}>
         <Map
-          places={places}
+          places={place ? [place] : []}
           styles={{ height: '90vh' }}
           zoom={17}
-          center={places.length > 0 ? [places[0].latitude, places[0].longitude] : [35.69, 135.69]}
+          center={place ? [place.latitude, place.longitude] : [35.69, 135.69]}
         />
       </div>
     </HomeLayout>
