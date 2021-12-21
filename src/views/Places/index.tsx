8ec11810d3e1d7a8f6ed964e52d2b6d03c38a78f
wzkMaster <727498469@qq.com>
2021-12-21 20:33:07 +0800

    feat(Places): 初始化地点页面

---
+++ src/views/Places/index.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import style from './index.less';
+import Request from '@/request';
+import Map from '@/components/Map';
+import HomeLayout from '@/layouts/HomeLayout';
+
+const Places: React.FC<{}> = () => {
+  const [places, setPlaces] = useState<IPlaceInfoBrief[]>([]);
+  useEffect(() => {
+    Request.getPlaces().then((res) => {
+      const { data } = res;
+      setPlaces(
+        data.results.map((place) => {
+          return {
+            id: place.id,
+            name: place.name,
+            address: place.address,
+            longitude: place.longitude,
+            latitude: place.latitude,
+            photos: place.photos,
+          };
+        }),
+      );
+    });
+  }, []);
+
+  return (
+    <HomeLayout>
+      <div className={style.container}>
+        <Map places={places} styles={{ height: '90vh' }} zoom={7} center={[35.69, 135.69]} />
+      </div>
+    </HomeLayout>
+  );
+};
+
+export default Places;
