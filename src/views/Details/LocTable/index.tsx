dddedc56c9f84a6db8ac6502eba1f7c33aaca356
wzkMaster <727498469@qq.com>
2021-12-22 09:53:27 +0800

    feat(Details): 地点列表增加跳转地点详情链接

--- src/views/Details/LocTable/index.tsx
+++ src/views/Details/LocTable/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import style from './index.less';
 import NoData from '@/components/NoData';
 
@@ -20,20 +21,24 @@ const LocTable: React.FC<ILocTableProps> = ({ places }) => {
       <table className={style.table}>
         <thead>
           <tr className={style.head}>
-            <th>地点ID</th>
             <th>场景名称</th>
             <th>详细地址</th>
             <th>坐标位置</th>
+            <th> </th>
           </tr>
         </thead>
         <tbody>
           {places.map((place, index) => {
             return (
               <tr key={index}>
-                <td>{place.id}</td>
                 <td>{place.name}</td>
                 <td>{place.address}</td>
                 <td>{place.latitude + ', ' + place.longitude}</td>
+                <td>
+                  <Link to={`/places/${place.id}`}>
+                    <span className={style.link}>详情</span>
+                  </Link>
+                </td>
               </tr>
             );
           })}
