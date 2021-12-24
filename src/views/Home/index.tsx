a14ecda7a3f70265fb22012e81179fff26c1ea0f
NagisaCo <yjr888@vip.qq.com>
2021-12-24 23:24:51 +0800

    refactor(reco): 重构推荐部分请求

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -11,22 +11,39 @@ const Home: React.FC = () => {
   const { page } = useParams();
   const navigate = useNavigate();
   const [totalPages, setTotalPages] = React.useState(0);
-  const [recommendList, setRecommendList] = React.useState<IRecommendInfo[]>([]);
+  const [recommendTable, setRecommendTable] = React.useState<IRecommendInfo[]>([]);
+  const [recommendCarousel, setRecommendCarousel] = React.useState<IRecommendInfo[]>([]);
+
+  useEffect(() => {
+    // 初始化加载轮播图
+    Request.getRecommendPage(Number(page), 4, 'CAROUSEL', 'score', false)
+      .then((res) => {
+        const { data } = res;
+        console.log('获取RecommendCarouselPage成功', data);
+        setRecommendCarousel(data.results);
+        console.log(recommendCarousel)
+      })
+      .catch((err) => {
+        console.log('获取RecommendCarouselPage失败', err);
+      });
+  }, []);
 
   const onCurrentPageChange = (page: number) => {
     navigate(`/${page}`);
   };
 
   useEffect(() => {
-    Request.getRecommendationList(1) // TODO: 数据不够，暂时只请求第一页
+    // TODO: 数据不够，暂时只请求第一页
+    Request.getRecommendPage(Number(page), 8, 'TABLE', 'score', false)
       .then((res) => {
-        console.log('获取RecommendationList成功', res);
         // setTotalPages(Math.ceil(res.count / 20));
+        const { data } = res;
+        console.log('获取RecommendTablePage成功', data);
         setTotalPages(16); // TODO: 假设有好多页数据
-        setRecommendList(res.results);
+        setRecommendTable(data.results);
       })
       .catch((err) => {
-        console.log('获取RecommendationList失败', err);
+        console.log('获取RecommendTablePage失败', err);
       });
   }, [page]);
 
@@ -39,7 +56,7 @@ const Home: React.FC = () => {
           </div>
         </div>
         <div>
-          <RecommendationTable recommendList={recommendList} />
+          <RecommendationTable recommendList={recommendTable} />
           <PageController
             currentPage={page ? Number(page) : 1}
             totalPages={totalPages}
