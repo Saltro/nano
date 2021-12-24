8abe2c4e3c1951ef38c3aeb95058483e786f7a55
NagisaCo <yjr888@vip.qq.com>
2021-12-25 00:25:39 +0800

    feat(reco): 首页轮播图展示

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,27 +1,29 @@
 import React, { useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams, Link } from 'react-router-dom';
 import Request from '@/request';
 import HomeLayout from '@/layouts/HomeLayout';
 import PageController from '@/components/PageController';
 import SearchBox from '@/components/SearchBox';
 import RecommendationTable from './RecommendTable';
 import style from './index.less';
+import { Carousel } from 'antd';
 
 const Home: React.FC = () => {
   const { page } = useParams();
   const navigate = useNavigate();
+  const RecommendTablePageSize = 8;
+  const RecommendCarouselPageSize = 4;
   const [totalPages, setTotalPages] = React.useState(0);
   const [recommendTable, setRecommendTable] = React.useState<IRecommendInfo[]>([]);
   const [recommendCarousel, setRecommendCarousel] = React.useState<IRecommendInfo[]>([]);
 
   useEffect(() => {
     // 初始化加载轮播图
-    Request.getRecommendPage(Number(page), 4, 'CAROUSEL', 'score', false)
+    Request.getRecommendPage(Number(page), RecommendCarouselPageSize, 'CAROUSEL', 'score', false)
       .then((res) => {
         const { data } = res;
         console.log('获取RecommendCarouselPage成功', data);
         setRecommendCarousel(data.results);
-        console.log(recommendCarousel)
       })
       .catch((err) => {
         console.log('获取RecommendCarouselPage失败', err);
@@ -34,9 +36,9 @@ const Home: React.FC = () => {
 
   useEffect(() => {
     // TODO: 数据不够，暂时只请求第一页
-    Request.getRecommendPage(Number(page), 8, 'TABLE', 'score', false)
+    Request.getRecommendPage(Number(1), RecommendTablePageSize, 'TABLE', 'score', false)
       .then((res) => {
-        // setTotalPages(Math.ceil(res.count / 20));
+        // setTotalPages(Math.ceil(res.count / RecommendTablePageSize));
         const { data } = res;
         console.log('获取RecommendTablePage成功', data);
         setTotalPages(16); // TODO: 假设有好多页数据
@@ -56,6 +58,20 @@ const Home: React.FC = () => {
           </div>
         </div>
         <div>
+          <div className={style.carousel}>
+            <Carousel autoplay>
+              <div className={style.slide}>
+                <Link to={`/detail/${recommendCarousel[0]?.anime}`}>
+                  <div className={style.description}>
+                    {recommendCarousel[0]?.description}
+                  </div>
+                  <div className={style.imgContainer}>
+                    <img src={recommendCarousel[0]?.image}/>
+                  </div>
+                </Link>
+              </div>
+            </Carousel>
+          </div>
           <RecommendationTable recommendList={recommendTable} />
           <PageController
             currentPage={page ? Number(page) : 1}
