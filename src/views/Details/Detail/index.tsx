9cffa2721c12d0a5949b461c617fe075638fe1da
wzkMaster <727498469@qq.com>
2021-12-25 21:27:58 +0800

    feat(Details): 优化标签样式

--- src/views/Details/Detail/index.tsx
+++ src/views/Details/Detail/index.tsx
@@ -57,15 +57,15 @@ const Detail: React.FC<IDetailProps> = (props) => {
       <div className={style.details}>
         <img src={image} className={style.image} />
         <div className={style.info}>
-          <p>导演 : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{director.join(' / ')}</p>
-          <p>原作 : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{origin.join(' / ')}</p>
-          <p>分镜 : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{storyboard.join(' / ')}</p>
-          <p>脚本 : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{script.join(' / ')}</p>
-          <p>音乐 : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{music.join(' / ')}</p>
-          <p>制作 : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{producer.join(' / ')}</p>
-          <p>演员 : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{actors.join(' / ')}</p>
-          <p>制片国家 : {country}</p>
-          <p>其他名称 : {alias.join(' / ')}</p>
+          {director.length > 0 && <p><span className={style.filed}>导演 : </span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{director.join(' / ')}</p>}
+          {origin.length > 0 && <p><span className={style.filed}>原作 : </span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{origin.join(' / ')}</p>}
+          {storyboard.length > 0 && <p><span className={style.filed}>分镜 : </span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{storyboard.join(' / ')}</p>}
+          {script.length > 0 && <p><span className={style.filed}>脚本 : </span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{script.join(' / ')}</p>}
+          {music.length > 0 && <p><span className={style.filed}>音乐 : </span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{music.join(' / ')}</p>}
+          {producer.length > 0 && <p><span className={style.filed}>制作 : </span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{producer.join(' / ')}</p>}
+          {actors.length > 0 && <p><span className={style.filed}>演员 : </span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{actors.join(' / ')}</p>}
+          {country.length > 0 && <p><span className={style.filed}>制片国家 : </span>{country}</p>}
+          {alias.length > 0 && <p><span className={style.filed}>其他名称 : </span>{alias.join(' / ')}</p>}
         </div>
       </div>
       <div className={style.description}>
