997724d14012951d865c1e157283905036f5fc6e
saltro <2373117747@qq.com>
2021-12-25 23:24:05 +0800

    fix(request place): 修复了找不到getPlacePage和getPlace的bug

--- src/request/place.ts
+++ src/request/place.ts
@@ -4,6 +4,19 @@ const getAnimePlaces = (id: string) => {
   return request.get<IPageInfo<IPlaceInfo>>(`/place?anime_id=${id}`);
 };
 
+const getPlacePage = (page?: number, pageSize?: number) => {
+  return request.get<IPageInfo<IPlaceInfo>>(`/place/`, {
+    params: {
+      page,
+      page_size: pageSize,
+    },
+  });
+};
+
+const getPlace = (id: string | undefined) => {
+  return request.get<IPlaceInfo>(`/place/${id}`);
+};
+
 const checkPlaceCollection = (id: number | undefined) => {
   return request.get(`/place/${id}/collection/`);
 };
@@ -16,4 +29,11 @@ const deletePlaceCollection = (id: number | undefined) => {
   return request.delete(`/place/${id}/collection/`);
 };
 
-export default { getAnimePlaces, checkPlaceCollection, addPlaceCollection, deletePlaceCollection };
+export default {
+  getAnimePlaces,
+  getPlacePage,
+  getPlace,
+  checkPlaceCollection,
+  addPlaceCollection,
+  deletePlaceCollection,
+};
