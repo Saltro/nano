d30c4b7b02f06f4e895db249244dbbbffb70ff76
saltro <2373117747@qq.com>
2021-12-25 12:46:42 +0800

    feat: 增加loading组件；为home和work页面配置loading

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,29 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import Request from '@/request';
 import HomeLayout from '@/layouts/HomeLayout';
 import PageController from '@/components/PageController';
 import SearchBox from '@/components/SearchBox';
-import RecommendationTable from './RecommendTable';
+import Loading from '@/components/Loading';
+import RecommendTable from './RecommendTable';
 import style from './index.less';
 
 const Home: React.FC = () => {
   const { page } = useParams();
   const navigate = useNavigate();
-  const [totalPages, setTotalPages] = React.useState(0);
-  const [recommendList, setRecommendList] = React.useState<IRecommendInfo[]>([]);
+  const [totalPages, setTotalPages] = useState(0);
+  const [recommendList, setRecommendList] = useState<IRecommendInfo[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const onCurrentPageChange = (page: number) => {
     navigate(`/${page}`);
   };
 
   useEffect(() => {
+    setIsLoading(true);
     Request.getRecommendationList(1) // TODO: 数据不够，暂时只请求第一页
       .then((res) => {
         console.log('获取RecommendationList成功', res);
         // setTotalPages(Math.ceil(res.count / 20));
         setTotalPages(16); // TODO: 假设有好多页数据
         setRecommendList(res.results);
+        setIsLoading(false);
       })
       .catch((err) => {
         console.log('获取RecommendationList失败', err);
@@ -38,14 +42,14 @@ const Home: React.FC = () => {
             <SearchBox />
           </div>
         </div>
-        <div>
-          <RecommendationTable recommendList={recommendList} />
+        <Loading isLoading={isLoading}>
+          <RecommendTable recommendList={recommendList} />
           <PageController
             currentPage={page ? Number(page) : 1}
             totalPages={totalPages}
             onCurrentPageChange={onCurrentPageChange}
           />
-        </div>
+        </Loading>
       </div>
     </HomeLayout>
   );
