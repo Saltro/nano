6104b077aa1ef7528bc377384afd1c534ebe5afc
wzkMaster <727498469@qq.com>
2021-12-29 13:25:11 +0800

    feat(Detail): 轮播图指示点样式修改

--- src/views/Details/DoubleSwiper/index.tsx
+++ src/views/Details/DoubleSwiper/index.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { createRef } from 'react';
 import style from './index.less';
 import { Carousel, Image } from 'antd';
 import NoData from '@/components/NoData';
+import { CarouselRef } from 'antd/es/carousel';
 
 interface IDoubleSwiperProps {
   pictures: string[];
 }
 
 const DoubleSwiper: React.FC<IDoubleSwiperProps> = ({ pictures }) => {
+  const ref = createRef<CarouselRef>();
+  const [currentSlide, setCurrentSlide] = React.useState(0);
+  const imageGroups = toGroup(pictures);
+
+  const goTo = (slide: number) => {
+    ref.current?.goTo(slide, false);
+    setCurrentSlide(slide);
+  };
+
+  const handleSlideChange = (_currentSlide: number, nextSlide: number) => {
+    setCurrentSlide(nextSlide);
+  };
+
   if (pictures.length === 0) {
     return (
       <NoData
@@ -19,7 +33,7 @@ const DoubleSwiper: React.FC<IDoubleSwiperProps> = ({ pictures }) => {
   return (
     <div className={style.container}>
       <Image.PreviewGroup>
-        <Carousel autoplay>
+        <Carousel autoplay dots={false} ref={ref} beforeChange={handleSlideChange}>
           {toGroup(pictures).map((group, index) => {
             return (
               <div key={index} className={style.slide}>
@@ -40,6 +54,23 @@ const DoubleSwiper: React.FC<IDoubleSwiperProps> = ({ pictures }) => {
           })}
         </Carousel>
       </Image.PreviewGroup>
+      <div>
+        {imageGroups.length > 1 && (
+          <div className={style.slideBottomBox}>
+            {Array.from({ length: imageGroups.length }).map((_, index) => {
+              return (
+                <span
+                  key={index}
+                  className={index === currentSlide ? style.slideBottomActive : style.slideBottom}
+                  onClick={() => {
+                    goTo(index);
+                  }}
+                />
+              );
+            })}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
