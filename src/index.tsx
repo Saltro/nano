0c668d4fc5edc807194346f2949d4235d792f93f
saltro <2373117747@qq.com>
2022-01-03 20:07:41 +0800

    feat: 增加sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
+import { sentryDsn } from './secret';
 import App from './App';
+
+Sentry.init({
+  dsn: sentryDsn,
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <Router>
