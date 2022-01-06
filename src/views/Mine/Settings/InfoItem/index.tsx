3263e0269e0c89ad59a69584cee781d51dc15c7d
saltro <2373117747@qq.com>
2022-01-06 16:52:03 +0800

    feat(mine): 完成个人页面基本信息的样式

---
+++ src/views/Mine/Settings/InfoItem/index.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import style from './index.less';
+
+export interface IInfoItemProps {
+  title: string;
+  value?: string;
+  editable?: boolean;
+  onChange?: (value: string) => void;
+  editLabel?: string;
+}
+
+const InfoItem: React.FC<IInfoItemProps> = ({
+  title,
+  value = '暂无',
+  editable = false,
+  onChange,
+  editLabel = '编辑',
+}) => {
+  const [valueFake, setValueFake] = useState(value);
+  const [isEditShowing, setIsEditShowing] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+
+  return (
+    <div
+      className={style.info}
+      onMouseEnter={() => setIsEditShowing(true)}
+      onMouseLeave={() => setIsEditShowing(false)}
+    >
+      <div className={style.infoTitle}>
+        <span>{title}</span>
+        {editable && (
+          <span
+            style={{ opacity: isEditShowing ? 1 : 0 }}
+            className={style.edit}
+            onClick={() => {
+              setIsEditShowing(false);
+              setIsEditing(true);
+            }}
+          >
+            {editLabel}
+          </span>
+        )}
+      </div>
+      {(!editable || !isEditing) && <p className={style.value}>{value}</p>}
+      {editable && isEditing && (
+        <input
+          className={style.input}
+          type="text"
+          value={valueFake}
+          onChange={(e) => {
+            setValueFake(e.target.value);
+          }}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              setIsEditing(false);
+              onChange?.(valueFake);
+            }
+          }}
+        />
+      )}
+    </div>
+  );
+};
+
+export default InfoItem;
--- src/views/Mine/Settings/index.tsx
+++ src/views/Mine/Settings/index.tsx
@@ -1,20 +1,68 @@
 import React from 'react';
-import { useAuth } from '@/context/AuthContainer';
 import { useNavigate } from 'react-router-dom';
+import { useAuth } from '@/context/AuthContainer';
+import InfoItem, { IInfoItemProps } from './InfoItem';
+import style from './index.less';
 
 const Settings: React.FC = () => {
   const auth = useAuth();
   const navigate = useNavigate();
 
+  const infoItems: IInfoItemProps[] = [
+    {
+      title: '用户名',
+      value: auth?.userInfo?.username,
+    },
+    {
+      title: '昵称',
+      value: auth?.userInfo?.nickname,
+      editable: true,
+    },
+    {
+      title: '绑定手机',
+      value: '+86 ' + auth?.userInfo?.mobile,
+      editable: true,
+      editLabel: '切换手机号',
+    },
+    {
+      title: '绑定邮箱',
+      editable: true,
+      editLabel: '立刻绑定',
+    },
+    {
+      title: '密码',
+      value: '已设置',
+      editable: true,
+      editLabel: '修改密码',
+    },
+  ];
+
   return (
-    <button
-      onClick={() => {
-        auth?.logout();
-        navigate('/');
-      }}
-    >
-      登出账号
-    </button>
+    <div id={style.container}>
+      <div id={style.infoContainer}>
+        <p className={style.title}>基本信息</p>
+        <div>
+          {infoItems.map((item, index) => (
+            <InfoItem key={index} {...item} />
+          ))}
+        </div>
+      </div>
+      <div id={style.interestContainer}>
+        <p className={style.title}>兴趣标签</p>
+      </div>
+      <div id={style.authContainer}>
+        <p className={style.title}>登录状态</p>
+        <button
+          className={style.warnBtn}
+          onClick={() => {
+            auth?.logout();
+            navigate('/');
+          }}
+        >
+          退出登录
+        </button>
+      </div>
+    </div>
   );
 };
 
